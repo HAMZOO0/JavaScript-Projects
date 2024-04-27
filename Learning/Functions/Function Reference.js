528a64a73585a4345a347e5088da70933abbc9c0
Hamza Sajid <98114762+HAMZOO0@users.noreply.github.com>
2024-04-27 12:23:59 +0500

    Create Function Reference.js

---
+++ Learning/Functions/Function Reference.js
@@ -0,0 +1,9 @@
+function greet() {
+  console.log("Hello!");
+}
+
+// Calling the function
+greet(); // This will print "Hello!"
+
+// Referring to the function
+console.log(greet); // This will print the function definition itself
