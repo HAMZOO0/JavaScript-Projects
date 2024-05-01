f1be4be8f0b01b2f32680b81487b3fde527721cb
Hamza Sajid <98114762+HAMZOO0@users.noreply.github.com>
2024-05-01 11:36:22 -0700

    Add files via upload

--- Project/GitHub Account tracker/script.js
+++ Project/GitHub Account tracker/script.js
@@ -1,4 +1,4 @@
-let api_url = "https://api.github.com/users/"
+//let api_url = "https://api.github.com/users/"
 
 const search_box = document.querySelector(".search_box")
 
@@ -10,8 +10,10 @@ const followers = document.querySelector(".followers")
 
 const repos = document.querySelector(".repo")
 
+const img = document.querySelector(".img_box")
 
- 
+
+
 
 // here we create function to attach the account name with api url to find account 
 function api_url_name_attacher(account_name) {
@@ -19,30 +21,30 @@ function api_url_name_attacher(account_name) {
     //console.log(api_url);
 }
 
-function ress () {
+function DATA_Access() {
     const xhr = new XMLHttpRequest();
 
-xhr.open("GET",api_url)
-xhr.send()
+    xhr.open("GET", api_url)
+    xhr.send()
 
-xhr.onload = function(){
-    if (xhr.status == 200) {
-        const data = JSON.parse(this.responseText)
-         
-        console.log(data)
+    xhr.onload = function () {
+        if (xhr.status == 200) {
+            const data = JSON.parse(this.responseText)
 
-        // name.innerHTML += data
-        // followers.innerHTML += data.
-        // repos.innerHTML += data.results[0].
+            console.log(data)
 
-    }
+            name.innerHTML += data.name
+            followers.innerHTML += data.followers
+            repos.innerHTML += data.public_repos
+            img.innerHTML = `<img src="${data.avatar_url}" alt="pic" width="255px">`
 
-    else{
-    console.log("Error");
+        }
+
+        else {
+            console.log("Error");
+        }
     }
 }
-}
-ress()
 
 
 // here we accept input form user which is username of github 
@@ -55,7 +57,7 @@ search_box.addEventListener("submit", function (e) { // submit : when we press e
 
     api_url_name_attacher(account_name)
 
-    ress()
+    DATA_Access()
 })
 
 //------------------------------
