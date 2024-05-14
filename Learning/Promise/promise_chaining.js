af097c7e0d73a14e955f11b80b0741d99be772e9
Hamza Sajid <98114762+HAMZOO0@users.noreply.github.com>
2024-05-14 11:36:58 +0500

    Update promise_chaining.js

--- Learning/Promise/promise_chaining.js
+++ Learning/Promise/promise_chaining.js
@@ -15,3 +15,30 @@ getData(1)
     .then((res) => {
         console.log(res);
     });
+
+
+// ===============================================
+
+/*
+
+----------------------------------------------------------------------------------
+fetch('https://cat-fact.herokuapp.com/facts')
+
+.then((response)=>{
+    response.json()
+    console.log(response[0].text);
+})
+
+here i reserved error becuse i use console.log(response[0].text); before converting into js obejct 
+
+*/
+
+// we use 2nd then bcz 1s we want to resolve this reposne.json after this next opeartion will work or we say that we pause it
+
+fetch('https://cat-fact.herokuapp.com/facts')
+    .then((response) => {
+        return response.json();
+    })
+    .then((data) => {
+        console.log(data[0].text);
+    });
